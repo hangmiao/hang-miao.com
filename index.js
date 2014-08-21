ee81c28c5c7f92efbc97e6f562ac6f9fe85d1b34
hahnmiao <hahnmiao@gmail.com>
2014-08-21 01:54:52 -0700

    add drop down menu

---
+++ index.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    // arrow in the submenu
+//    $("#ar").html("&#9652;");
+//    $("#ar:first-child").addClass("intro");
+    $("#ar").html("&#9652;");
+    $("#ar").addClass("intro");
+
+//  dropdown menu animation
+    $("li").hover(function() {
+        // $(this).find("ul>li").stop().slideToggle(300);
+        $(this).find("ul>li, #ar").stop().fadeToggle(200);
+    });
+});
